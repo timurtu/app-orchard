d2d2251b66f8bc9ee24b0404bdef9ef88bca2031
Timur Uzel <timurt.uzel@gmail.com>
2016-07-26 12:57:47 -0400

    coolish navbar

--- src/app.js
+++ src/app.js
@@ -2,7 +2,27 @@
  * Created by timur on 7/26/16.
  */
 
-import './style.scss'
+import './styles/main.scss'
+// import './styles/fonts.scss'
 
-document.write('holy smokes')
+import dom from 'domali'
+
+const [app, nav, title, wrapper, p] =
+  dom.create('app', 'nav', 'h1', 'div', 'p')
+
+title.appendChild(document.createTextNode('App Orchard'))
+
+nav.appendChild(title)
+app.appendChild(nav)
+
+p.appendChild(document.createTextNode(
+  'Welcome to the App Orchard. To get started, create a new App Idea.'
+))
+
+wrapper.set({ class: 'wrapper' })
+  .appendChild(p)
+
+app.appendChild(wrapper)
+
+dom.render(app)
 
