8c37b05ad3b7fcfef4e076ac56c37a04cb41630c
Timur Uzel <timurt.uzel@gmail.com>
2016-08-01 19:42:03 -0400

    kidding myself by saying this is less decoupled or something

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -12,20 +12,25 @@ export class Posts extends React.Component {
     return (
       <ul>
         {
-          ideas.map(idea =>
-            <li key={idea.id} className="post">
-              <h1>{idea.name}</h1>
-              <small className="stars">{idea.stars} stars</small>
-              <small>Posted by {idea.author}</small>
-              <div className="image">
-                <img src="http://lorempixel.com/300/120"/>
-              </div>
-              <p>{idea.info}</p>
-              <small className="comments">{idea.stars} comments</small>
-            </li>
-          )
+          this._getPosts()
         }
       </ul>
     )
   }
+  
+  _getPosts() {
+    return ideas.map(idea =>
+      <li key={idea.id} className="post">
+        <h1>{idea.name}</h1>
+        <small className="stars">{idea.stars} stars</small>
+        <small>Posted by {idea.author}</small>
+        <div className="image">
+          <img src="http://lorempixel.com/300/120"/>
+        </div>
+        <p>{idea.info}</p>
+        <small className="comments">{idea.stars} comments</small>
+      </li>
+    )
+  }
+  
 }
