b55e3b47e328fe754d3c687f6d2a49303edf700a
Timur Uzel <timurt.uzel@gmail.com>
2016-07-31 23:34:34 -0400

    lookinggood

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -17,8 +17,9 @@ export class Posts extends React.Component {
               <h1>{idea.name}</h1>
               <small className="stars">{idea.stars} stars</small>
               <small>Posted by {idea.author}</small>
-              <div className="image"></div>
-              <img src="http://lorempixel.com/300/100"/>
+              <div className="image">
+                <img src="http://lorempixel.com/300/100"/>
+              </div>
               <p>{idea.info}</p>
               <small className="comments">{idea.stars} comments</small>
             </li>
