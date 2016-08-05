495c8b377b194f39a8f08e7dd32a3995564551d1
Timur Uzel <timurt.uzel@gmail.com>
2016-08-05 01:29:35 -0400

    added action constants to tests

--- src/reducers/__tests__/counter.test.js
+++ src/reducers/__tests__/counter.test.js
@@ -2,32 +2,33 @@
  * Created by timur on 8/4/16.
  */
 
-jest.unmock('../counter')
 jest.unmock('chai')
+jest.unmock('../counter')
+jest.unmock('../../constants/actions')
 
 import { expect } from 'chai'
 import counter from '../counter'
-
+import { add, subtract } from '../../constants/actions'
 
 describe('counter reducer', function () {
   
-  describe('add', function () {
+  describe('add action', function () {
     
-    it('should add a value to the state', function() {
-      expect(counter(0, { type: 'add' })).to.equal(1)
-      expect(counter(1, { type: 'add' })).to.equal(2)
+    it('should add a value to the state', function () {
+      expect(counter(0, add)).to.equal(1)
+      expect(counter(1, add)).to.equal(2)
     })
   })
   
-  describe('subtract', function () {
+  describe('subtract action', function () {
     
-    it('should remove a value from the state', function() {
-      expect(counter(2, { type: 'subtract' })).to.equal(1)
-      expect(counter(1, { type: 'subtract' })).to.equal(0)
+    it('should remove a value from the state', function () {
+      expect(counter(2, subtract)).to.equal(1)
+      expect(counter(1, subtract)).to.equal(0)
     })
   })
   
-  describe('default', function () {
+  describe('default action', function () {
     
     it('should return the same state', function () {
       expect(counter(0, { type: 'other' })).to.equal(0)
